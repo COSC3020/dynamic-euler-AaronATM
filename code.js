bc6f3467c25aeb61944e52069f4487d6e0be56dd
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-05 14:58:19 -0600

    updated code.js

--- code.js
+++ code.js
@@ -1,9 +1,35 @@
+var fact = [];
+fact[0] = 1;
+
 function factorial(n) {
-    if(n === 0) return 1;
-    else return n * factorial(n - 1);
+    if(n === 0)
+    {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
 }
 
 function e(n) {
-    if(n === 0) return 1;
-    else return 1.0 / factorial(n) + e(n - 1);
+    var result = 0;
+
+    if(n === 0)
+    {
+        return 1;
+    }
+
+    for (var i = 0; i <= n; i++)
+    {
+        if (fact[i] === undefined)
+        {
+            fact[i] = factorial(i);
+        }
+
+        result = result + (1 / fact[i]);
+    }
+
+    // repeated work:
+    // else return 1.0 / factorial(n) + e(n - 1);
+
+    return result;
 }
