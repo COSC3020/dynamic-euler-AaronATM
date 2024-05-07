92ae8daed56948b94aae709ac3bd9689ce4eab0f
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-06 19:34:35 -0600

    updated code.js

--- code.js
+++ code.js
@@ -1,10 +1,5 @@
 // using techniques from lecture
 
-/*
-repeated work:
-    else return 1.0 / factorial(n) + e(n - 1);
-
-    and
 function factorial(n) {
     if(n === 0)
     {
@@ -13,7 +8,6 @@ function factorial(n) {
 
     return n * factorial(n - 1);
 }
-*/
 
 function e(n) {
     var result = 1, tmp = 1;
@@ -25,8 +19,13 @@ function e(n) {
 
     for (var i = 1; i <= n; i++)
     {
+        // can do it without another function call
+        // based on testing code I am not sure if that is what you want
         tmp = tmp * i;
         result = result + (1.0 / tmp);
+
+        // another option would be to push values of the previous factorial to an array
+        // which is basically memoization, hence why I didnt use it
     }
 
     return result;
