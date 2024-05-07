4a2b338e257d759e2650e929c16470a864ffa493
Aaron Krapes <aaronkrapes@outlook.com>
2024-05-06 19:30:02 -0600

    updated code.js

--- code.js
+++ code.js
@@ -1,5 +1,10 @@
 // using techniques from lecture
 
+/*
+repeated work:
+    else return 1.0 / factorial(n) + e(n - 1);
+
+    and
 function factorial(n) {
     if(n === 0)
     {
@@ -8,22 +13,21 @@ function factorial(n) {
 
     return n * factorial(n - 1);
 }
+*/
 
 function e(n) {
-    var result = 0;
+    var result = 1, tmp = 1;
 
     if(n === 0)
     {
         return 1;
     }
 
-    for (var i = 0; i <= n; i++)
+    for (var i = 1; i <= n; i++)
     {
-        result = result + (1.0 / factorial(i));
+        tmp = tmp * i;
+        result = result + (1.0 / tmp);
     }
-
-    // repeated work:
-    // else return 1.0 / factorial(n) + e(n - 1);
 
     return result;
 }
